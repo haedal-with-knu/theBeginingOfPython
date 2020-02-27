def5614b3dae55ccb936b63ae41dacfae842721f
ewsn1593 <ewsn1593@gmail.com>
2020-02-28 00:58:50 +0900

    complete code & document

--- 6.db+crawling/melon_rank.py
+++ 6.db+crawling/melon_rank.py
@@ -1,38 +1,15 @@
-# 이 코드도 왜 안돌아가냐 ㅠㅠ 조금만 고치면 작동할듯
-'''
-import requests
-import bs4
-import json
-import csv
+import bs4 #pip install bs4
+import requests #pip install requests
 
-headers ={
-  'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Mobile Safari/537.36'
-}
-
-response = requests.get('https://www.melon.com/chart/index.htm', headers=headers).text
-print(response)
-#mobile_response = requests.get('https://m.app.melon.com/index.htm', headers=headers).text
-soup = bs4.BeautifulSoup(response, 'html.parser')
-songs = soup.select('#lst50')
-
-with open('melon_rank.csv', 'w', encoding='utf-8') as f:
-  for song in songs:
-    rank = song.select_one('td:nth-child(2) > div > span.rank').text
-    title = song.select_one('td:nth-child(6) > div > div > div.ellipsis.rank01 > span > a').text
-    artist = song.select_one('#lst50 > td:nth-child(6) > div > div > div.ellipsis.rank02 > a').text
-    f.write(f'{rank}위,{title},{artist}\n')
-'''
-
-import bs4
-import requests
 headers ={
   'User-Agent': 'Not_Crawling X)'
 }
+
 response = requests.get('https://www.melon.com',headers=headers).text
 soup = bs4.BeautifulSoup(response, 'html.parser')
 songs = soup.select('.on.nth1 >.list_wrap.typeRealtime>ul>.rank_item')
 
-with open('melon_rank.csv', 'w', encoding='utf-8') as f:
+with open('melon_rank.csv', 'w') as f:
   for song in songs:
     rank = song.select_one('div.rank_number > span.rank').text
     title = song.select_one('div.rank_cntt > div.rank_info > p.song > a').text
--- 6.db+crawling/naver_rank.py
+++ 6.db+crawling/naver_rank.py
@@ -1,19 +1,11 @@
 import requests # pip install requests
-import bs4 # pip install bs4
 import datetime
 import json
 
 data = requests.get('https://www.naver.com/srchrank?frm=main&ag=all&gr=1&ma=-2&si=0&en=0&sp=0').text
 data = json.loads(data)
-#soup = bs4.BeautifulSoup(html, 'html.parser')
-#print(soup)
 
-# 이렇게 실시간 검색어 긁어오면 되는데 잘 안되네요 ㅠㅠ 확인 부탁합니다
-# ranks = soup.select('.PM_CL_realtimeKeyword_rolling .ah_item .ah_k')
-# ranks = soup.select("#NM_RTK_VIEW_list_wrap .ah_item .ah_a")
 ranks = data['data']
-print(ranks)
-
 
 now = datetime.datetime.now()
 
